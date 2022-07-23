328a6b578d05c51ef0266a373b58b372ddefea6d
Creative Rigor, LLC <creativerigor@gmail.com>
2022-07-23 12:19:42 -0400

    Added full deck footer and header configuration and styling.
    Also started documentation with a section on header and footer.

--- example_doc/conf.py
+++ example_doc/conf.py
@@ -40,7 +40,6 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = [
     '_build', 'Thumbs.db', '.DS_Store',
-    'content.md', '_keynote.md',
 ]
 
 
